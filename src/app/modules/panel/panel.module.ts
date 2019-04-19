1bf431396108e6b516c1f3a0250bd4dfd202a71f
Matteo Colombo <matte.colo@gmail.com>
2019-04-19 21:55:06 +0200

    Add panel module for competitions

--- src/app/modules/panel/panel.module.ts
+++ src/app/modules/panel/panel.module.ts
@@ -9,6 +9,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material';
 import { SharedComponentsModule } from '../shared-components/shared-components.module';
 import { FaqPanelComponent } from './faq-panel/faq-panel.component';
+import { CompPanelComponent } from './comp-panel/comp-panel.component';
 
 @NgModule({
   imports: [
@@ -18,6 +19,6 @@ import { FaqPanelComponent } from './faq-panel/faq-panel.component';
     MatListModule,
     SharedComponentsModule
   ],
-  declarations: [PanelComponent, ArticlePanelComponent, TeamPanelComponent, TutorialPanelComponent, FaqPanelComponent]
+  declarations: [PanelComponent, ArticlePanelComponent, TeamPanelComponent, TutorialPanelComponent, FaqPanelComponent, CompPanelComponent]
 })
 export class PanelModule { }
